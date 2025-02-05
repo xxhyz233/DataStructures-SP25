5c870bb6ec4d29c6d0981e60400c945febc292e9
XXHYZ <1727181899@qq.com>
2025-02-05 12:17:37 -0500

    Poopoo head 4.0

--- Week2/FibonacciRace/FibonacciRace.java
+++ Week2/FibonacciRace/FibonacciRace.java
@@ -1,12 +1,6 @@
 /* Homework assignment 1: Fibonacci Race
  */
 public class FibonacciRace {
-  // Just some colored ANSI code so it's less boring in cmd
-  public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_RED = "\u001B[31m";
-  public static final String ANSI_PURPLE = "\u001B[35m";
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_BLUE = "\u001B[36m";
   public static void main(String[] args) {
     // Setting our index as the first argument from cmd
     int maxInput = Integer.parseInt(args[0]);
