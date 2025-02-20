7c41f4f358ffce7b36d2f4dec72279edb4131f92
xxhyz233 <49100459+xxhyz233@users.noreply.github.com>
2025-02-19 22:12:05 -0500

    minor

--- Week5/QueueExample.java
+++ Week5/QueueExample.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 
-public class Stack {
+public class QueueExample {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
